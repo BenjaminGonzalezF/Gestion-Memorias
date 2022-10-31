0b2f36a412ede2398389bc987dd43e280cf5c5df
Sebastian Pino <seba.kodk@gmail.com>
2022-10-31 20:42:20 -0300

    Vista login
    Falta conectar ambos componentes creados y aplicar funcionalidades

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,19 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   },
 ]
 
---
+++ Frontend/src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- <h1>{{$store.state.hola}}</h1> -->
+  <!--Formulario ingreso-->
+  <div>
+    <BarraSuperior/>
+  <v-container>
+    <v-content>
+    <v-card
+    class="mx-auto"
+    max-width="460"
+    elevation="3"
+    outlined
+    >
+      <v-card-title>
+        Bienvenido
+      </v-card-title>
+      <v-card-text>
+        <v-text-field label="Usuario" prepend-icon="mdi-account-circle"/>
+
+        <v-text-field
+        label="Contraseña"
+        prepend-icon="mdi-lock"
+        >
+        </v-text-field>
+
+        <v-card-actions class="my-n4">
+          <v-card-text>
+            ¿No recuerdas tu contraseña?
+          </v-card-text >
+          <v-btn class="white--text" color="#65727c" small>Ingresar</v-btn>
+        </v-card-actions>
+        
+      </v-card-text>
+    </v-card>
+  </v-content>
+  </v-container>
+  </div>
+  
+</template>
+
+<script>
+  import BarraSuperior from './views/BarraSuperior.vue';
+  export default{
+    components:{
+      BarraSuperior,
+    }
+  };
+<script/>
