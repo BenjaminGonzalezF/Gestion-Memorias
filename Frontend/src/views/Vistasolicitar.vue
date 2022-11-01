58a0ec251577be2a15dd674d3a73003c023d5ce2
Cristian Fuentes <crfuentes19@alumnos.utalca.cl>
2022-11-01 18:11:16 -0400

    Mejora vista solicitar ramo

--- Frontend/src/views/Vistasolicitar.vue
+++ Frontend/src/views/Vistasolicitar.vue
@@ -1,92 +1,93 @@
 <template>
     <v-app>
         <div align="center" class="about2">
-            <v-col 
-            cols="50"
-        sm="100">
+            <v-col lass="px-4">
+            
             <h1>
                 Cursos pendientes
             </h1>
             <v-card
-                height="300" width="80%" outlined class="overflow-y-auto" max-height="290"
+                height="250" width="80%" outlined class="overflow-y-auto" max-height="240"
             >
-            <v-col>
-                <v-card>
-                    <h3>
-                        Semestre 9
-                    </h3>
-                    <v-card
-                    height="40%" width="95%"
-                    >
-                        <v-col>
-                        <v-checkbox
-                        label="Taller de Desarrollo de Software"
-                        > 
-                        </v-checkbox>
-                        <v-checkbox
-                        label="Seguridad informatica"
-                        > 
-                        </v-checkbox>
-                        <v-checkbox
-                        label="Electivo II"
-                        > 
-                        </v-checkbox>
-                        <v-checkbox
-                        label="Gestión de Proyectos Tecnológicos"
-                        > 
-                        </v-checkbox>
-                        <v-checkbox
-                        label="Gestión de Recursos Humanos"
-                        > 
-                        </v-checkbox>
-                        </v-col>
-                    </v-card>
-                    <h3>
-                        Semestre 10
-                    </h3>
-                    <v-card
-                    height="40%" width="95%" max-height="450"
-                    >
-                        <v-col>
-                        <v-checkbox
-                        label="Formulación de Proyecto de Titulación"
-                        > 
-                        </v-checkbox>
-                        <v-checkbox
-                        label="Electivo III"
-                        > 
-                        </v-checkbox>
-                        <v-checkbox
-                        label="Gestion de Inovación y Emprendimiento"
-                        > 
-                        </v-checkbox>
-                        </v-col>
-                    </v-card>
-                    <h3>
-                        Semestre 11
-                    </h3>
-                    <v-card
-                    height="40%" width="95%" max-height="450"
-                    >
-                        <v-col>
-                        <v-checkbox
-                        label="Proyecto de Titulación"
-                        > 
-                        </v-checkbox>
-                        <v-checkbox
-                        label="Electivo IV"
-                        > 
-                        </v-checkbox>
-                        </v-col>
-                    </v-card>
+              <v-col>
+                  <v-card>
+                      <h3>
+                          Semestre 9
+                      </h3>
+                      <v-card
+                      height="40%" width="95%"
+                      >
+                          <v-col>
+                          <v-checkbox
+                          label="Taller de Desarrollo de Software"
+                          > 
+                          </v-checkbox>
+                          <v-checkbox
+                          label="Seguridad informatica"
+                          > 
+                          </v-checkbox>
+                          <v-checkbox
+                          label="Electivo II"
+                          > 
+                          </v-checkbox>
+                          <v-checkbox
+                          label="Gestión de Proyectos Tecnológicos"
+                          > 
+                          </v-checkbox>
+                          <v-checkbox
+                          label="Gestión de Recursos Humanos"
+                          > 
+                          </v-checkbox>
+                          </v-col>
+                      </v-card>
+                      <h3>
+                          Semestre 10
+                      </h3>
+                      <v-card
+                      height="40%" width="95%" max-height="450"
+                      >
+                          <v-col>
+                          <v-checkbox
+                          label="Formulación de Proyecto de Titulación"
+                          > 
+                          </v-checkbox>
+                          <v-checkbox
+                          label="Electivo III"
+                          > 
+                          </v-checkbox>
+                          <v-checkbox
+                          label="Gestion de Inovación y Emprendimiento"
+                          > 
+                          </v-checkbox>
+                          </v-col>
+                      </v-card>
+                      <h3>
+                          Semestre 11
+                      </h3>
+                      <v-card
+                      height="40%" width="95%" max-height="450"
+                      >
+                          <v-col>
+                          <v-checkbox
+                          label="Proyecto de Titulación"
+                          > 
+                          </v-checkbox>
+                          <v-checkbox
+                          label="Electivo IV"
+                          > 
+                          </v-checkbox>
+                          </v-col>
+                      </v-card>
+                    
+                  </v-card>
                   
-                </v-card>
-            </v-col>
+              </v-col>
+             
             </v-card>
-            <v-spacer></v-spacer>
-
-            <v-container>
-                
+            <v-col>
+            
+            </v-col>
+            
             <v-row
                 align="center"
                 justify="space-around"
@@ -95,12 +96,16 @@
             <v-btn>Editar datos</v-btn>
             <v-btn>Guardar datos</v-btn>
             </v-row>
-        </v-container>
+
+          
+                
+           
+       
             <h1>
                 Información
             </h1>
             <v-card
-                height="120" width="80%" 
+              height="200" width="80%" 
             >
             <v-col>
                 <p>
@@ -113,10 +118,10 @@
                     Profesor Guia: nombre profesor
                 </p>
             </v-col>
-                
+              <v-btn>Enviar Solicitud</v-btn> 
             </v-card>
-            <v-btn>Enviar Solicitud</v-btn>  
-           </v-col>
+             
+          </v-col>
         </div>
     </v-app>
 </template>
