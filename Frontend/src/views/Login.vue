7eac23a6d70a9499620eb216b646786cd7e3297a
Sebastian Pino <seba.kodk@gmail.com>
2022-11-04 23:15:17 -0300

    Login con botones de navegacion
    El codigo del blur tambien esta listo, hay que buscar la imagen, asignarlo y calibrar la cantidad .

--- Frontend/src/views/Login.vue
+++ Frontend/src/views/Login.vue
@@ -2,6 +2,14 @@
   <!-- <h1>{{$store.state.hola}}</h1> -->
   <!--Formulario ingreso-->
   <div>
+    <v-card color="#00CCCC">
+      <v-card-text height="100%" >
+        <v-hover v-slot="{ hover }"><v-btn plain class="white--text" :style="{ 'background-color': hover ? '#FF0182' :  '#00CCCC'}">Alumno</v-btn></v-hover>
+        <v-hover v-slot="{ hover }"><v-btn plain class="white--text" :style="{ 'background-color': hover ? '#FF0182' :  '#00CCCC'}">Profesor</v-btn></v-hover>
+        <v-hover v-slot="{ hover }"><v-btn plain class="white--text" :style="{ 'background-color': hover ? '#FF0182' :  '#00CCCC'}">Directora</v-btn></v-hover>
+        <v-hover v-slot="{ hover }"><v-btn plain class="white--text" :style="{ 'background-color': hover ? '#FF0182' :  '#00CCCC'}">Comité</v-btn></v-hover>
+      </v-card-text>
+    </v-card>
     <v-container>
       <v-content>
         <v-card class="mx-auto" max-width="460" elevation="3" outlined>
@@ -18,7 +26,7 @@
               <v-card-text>
                 ¿No recuerdas tu contraseña?
               </v-card-text>
-              <v-btn class="white--text" color="#65727c" small>Ingresar</v-btn>
+              <v-btn class="white--text" color="#65727c" small @click="$store.state.alumno">Ingresar</v-btn>
             </v-card-actions>
 
           </v-card-text>
@@ -29,11 +37,25 @@
 
 </template>
 
+<style>
+  .fondo{
+    background-image: url("../assets/prueba.jpg");
+    height: 100%;
+    filter: blur(8px);
+    -webkit-filter: blur(8px);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+</style>
+
 <script>
-  import BarraSuperior from './views/BarraSuperior.vue';
   export default{
-    components:{
-      BarraSuperior,
+    data(){
+      return{
+        
+      }
     }
   };
 <script/>
