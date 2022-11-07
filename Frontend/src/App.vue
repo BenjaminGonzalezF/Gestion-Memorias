334d638ff3bce039e17dc71e2233ab425ae7e36b
Manuel <mangonzalez18@alumnos.utalca.cl>
2022-11-07 20:31:04 -0300

    Avance vistas Feature 3

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -6,14 +6,9 @@
       <router-view/>
     </v-main>
     <v-footer color="272727" padless>
-      <v-row justify="center" no-gutters>
-        <v-btn v-for="icono in iconos" :key="icono" color="white" text rounded class="my-2" icon>
-          <v-icon size="24px">{{icono}}</v-icon>
-        </v-btn>
-        <v-col class="grey py-4 text-center white--text" cols="12">
+      <v-col class="grey py-4 text-center white--text" cols="12">
           {{ new Date().getFullYear() }} — <strong>Utal</strong>
-        </v-col>
-      </v-row>
+      </v-col>
     </v-footer>
   </v-app>
 </template>
---
+++ Frontend/src/components/headerCurricular.vue
@@ -0,0 +1,14 @@
+<template>
+  
+
+</template>
+
+<script>
+  export default {
+    name: 'HeaderCurricular',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
